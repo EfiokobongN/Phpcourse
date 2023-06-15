b086b35a7fa928b7aa6c478bbf1bb9e8c4266fba
fa06 <mailpreciousnelson@gmail.com>
2023-06-15 16:51:06 +0000

    push to github

--- Project/login.php
+++ Project/login.php
@@ -4,13 +4,14 @@
     
     <div>
 <h1>Login</h1>
-<p>No Account? <a href="register.php">Register here!</a></p>
+
 
 <form action="" method="post">
 <input type="text" name="username" placeholder="Username">
 <input type="password" name="password" placeholder="PassWord">
     <button type="submit" name="submit">Login</button>
 </form>
+<p class="text">No Account? <a href="register.php">Register here!</a></p>
 </div>
 <?php
     require_once 'Includes/footer.php';
---
+++ Project/register.php
@@ -0,0 +1,19 @@
+<?php
+    require_once 'Includes/header.php';
+    ?>
+<div>
+<h1 class="reg">Register</h1>
+
+
+<form action="" method="post">
+<input type="text" name="username" placeholder="Username">
+<input type="text" name="email" placeholder="E-mail">
+<input type="password" name="password" placeholder="PassWord">
+<input type="password" name="comfirnpassword" placeholder="ConfirmPassWord">
+    <button type="submit" name="submit" class="btn">Register</button>
+</form>
+<p class="text">Alredy Have An Account? <a href="login.php">Login!</a></p>
+</div>
+<?php
+    require_once 'Includes/footer.php';
+    ?>
